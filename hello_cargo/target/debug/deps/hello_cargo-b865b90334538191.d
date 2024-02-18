8f3a31c5e63907198de36da512597f1d21514ec1
maxwelbm <maxwelbm@hotmail.com>
2024-02-18 16:50:20 -0300

    meeting the cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-b865b90334538191.d
@@ -0,0 +1,5 @@
+/Users/maxwel.mazur/Workspace/the_rust_programming_language/hello_cargo/target/debug/deps/hello_cargo-b865b90334538191: src/main.rs
+
+/Users/maxwel.mazur/Workspace/the_rust_programming_language/hello_cargo/target/debug/deps/hello_cargo-b865b90334538191.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/maxwel.mazur/Workspace/the_rust_programming_language/hello_cargo/target/debug/hello_cargo: /Users/maxwel.mazur/Workspace/the_rust_programming_language/hello_cargo/src/main.rs
